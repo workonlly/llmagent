b74a2db845d27ae24448bcafc32704ad469e591b
workonlly <workonlly335@gmail.com>
2025-10-20 03:47:47 +0530

    new theme

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,6 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        tertiary:"bg-primary/25 dark:bg-primary/30  text-primary shadow-xs hover:bg-primary/20 dark:hover:bg-primary/25",
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -68,7 +68,7 @@ export const ProjectView = ({ projectId }: Props) => {
                                 </TabsTrigger>
                             </TabsList>
                             <div className="ml-auto flex items-center gap-x-2">
-                                <Button asChild size="sm" variant="default">
+                                <Button asChild size="sm" variant="tertiary">
                                     <Link href="/pricing">
                                         <Crown className="size-4" /> Upgrade
                                     </Link>
