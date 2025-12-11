e95f73cef082e841603134782efb012131cb66dc
workonlly <workonlly335@gmail.com>
2025-12-11 08:30:41 +0530

    frefe

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,9 +5,7 @@ const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL
-}).$extends(withAccelerate())
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
